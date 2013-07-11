9c07a3daecf0838c6662b377e9ca4e9a098f32c8
Amit <lordamit@gmail.com>
2013-07-11 21:26:41 +0600

    Initial Commit

---
+++ TestProject/src/apackage/MyMain.java
@@ -0,0 +1,13 @@
+package apackage;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleClass sampleClass = new SampleClass();
+		sampleClass.aMethodThatReturnsVoid();
+		
+
+	}
+
+}
---
+++ TestProject/src/apackage/SampleClass.java
@@ -0,0 +1,8 @@
+package apackage;
+
+public class SampleClass {
+	public void aMethodThatReturnsVoid() {
+		System.out.println("Hi there. I am from aMethodThatReturnsVoid");
+	}
+
+}
